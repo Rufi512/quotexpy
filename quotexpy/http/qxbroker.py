5cc7f31afdeafa95bdc037f4447a8c3f7dc3e55a
Rufi <Rafajos9@gmail.com>
2024-06-26 22:50:15 -0400

    add option

--- quotexpy/http/qxbroker.py
+++ quotexpy/http/qxbroker.py
@@ -22,6 +22,7 @@ class Browser(object):
         try:
             chrome_options = uc.ChromeOptions()
             chrome_options.add_argument("--disable-extensions")
+            chrome_options.add_argument('--incognito')
             chrome_options.add_argument('--disable-application-cache')
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument("--no-sandbox")
